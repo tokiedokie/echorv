7238d0ea42e13bff43bf8bad13455b06fda0fce1
tokiedokie <42903136+tokiedokie@users.noreply.github.com>
2019-11-29 18:11:55 +0900

    return args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    for arg in &args {
+        println!("{}", arg);
+    }
 }
