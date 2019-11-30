58c34a09f90e4e582754dfa0dc81020faecec401
tokiedokie <42903136+tokiedokie@users.noreply.github.com>
2019-11-30 11:43:25 +0900

    multiple args in one line

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,5 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    for arg in &args {
-        println!("{}", arg);
-    }
+    println!("{}", args.join(" "));
 }
